11690fa097125cac753ea91c184d83720871c67d
YUNAKA <yunaka.dev@gmail.com>
2024-04-08 16:50:33 +0900

    chore : Proxy 설정

---
+++ app/constants.ts
@@ -0,0 +1,4 @@
+export const API_CALENDAR_REG = "calendar/register";
+export const API_CALENDAR_DATA = "calendar/data";
+export const API_SCHEDULE_CREATE = "schedule/create";
+export const API_SCHEDULE_DATA = "schedule/date";
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: process.env.BACKEND_SERVER + '/:path*',
+          },
+        ];
+      },
+};
 
 export default nextConfig;
