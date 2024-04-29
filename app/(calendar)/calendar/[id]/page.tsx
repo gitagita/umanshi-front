2f7b272c9523e0c31a4aba61c7231b9a49450583
YUNAKA <yunaka.dev@gmail.com>
2024-04-29 20:35:52 +0900

    feat : 이벤트 누적합 프로세스 구현 #9

--- app/(calendar)/calendar/[id]/page.tsx
+++ app/(calendar)/calendar/[id]/page.tsx
@@ -1,6 +1,7 @@
 import CalendarMerge from "@/components/calendar_merge";
 import { getCalendarMerge } from "@/utils/calendar-data";
 import { cookies } from "next/headers";
+import { Suspense } from "react";
 
 interface iParams { params: { id: string } }
 
@@ -8,14 +9,14 @@ export default async function CalendarMergePage({ params: { id } }: iParams) {
   const userCookie = cookies().get("user");
   let userInfo = { userId: '', nickname: '', calendarCode: '', calendarNm: '', isLeader: '' };
   if (userCookie) userInfo = JSON.parse(userCookie?.value || '');;
-
-  const mergeData = await getCalendarMerge(userInfo.calendarCode);
-  //console.log(mergeData);
+  const mergeData = await getCalendarMerge(id);
 
   return (
     <div>
       <h1>만날 날짜를 선택하세요</h1>
-      <CalendarMerge></CalendarMerge>
+      <Suspense fallback={<h1>Loading calendar info</h1>}>
+        <CalendarMerge params={{ id: id, userList: mergeData.data.userList }} ></CalendarMerge>
+      </Suspense>
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false, //../calendar/:id 페이지에서 테스트 시 false 로 설정 (개발 모드에서만 동작)
   async rewrites() {
     return [
       {
