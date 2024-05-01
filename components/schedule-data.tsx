6123061b920e9e279e0fda4287d553760cb28c9c
YUNAKA <yunaka.dev@gmail.com>
2024-05-01 12:18:08 +0900

    feat : 일정 페이지 데이터 조회

--- components/schedule-data.tsx
+++ components/schedule-data.tsx
@@ -1,4 +1,5 @@
 import { getCalendarData } from '@/utils/calendar-data';
+import {getScheduleData} from '@/utils/schedule-data';
 import ScheduleRange from "@/components/schedule-range";
 
 interface iParams { params: { id: string, calendarCode: string } }
@@ -6,10 +7,11 @@ interface iParams { params: { id: string, calendarCode: string } }
 export default async function ScheduleData({ params: { id, calendarCode } }: iParams) {
 
   const calendar = await getCalendarData(calendarCode);
+  const schedule = await getScheduleData(id, calendarCode);
 
   return (
     <div>
-      <ScheduleRange params={{ id: id, calendarCode: calendarCode, dateList: calendar.data.dateList}} />
+      <ScheduleRange params={{ id: id, calendarCode: calendarCode, dateList: calendar.data.dateList, scheduleList: schedule.data}} />
     </div>
   );
 }
